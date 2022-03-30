1a0abe3f1542cb86d971295e2738307e7edf67c0
haike <haike0513@gmail.com>
2022-03-30 10:11:56 +0800

    add build-tag

--- jest.config.cjs
+++ jest.config.cjs
@@ -4,7 +4,7 @@ const config = require('@polkadot/dev/config/jest.cjs');
 module.exports = {
   ...config,
   moduleNameMapper: {
-    '@qbit/sdk-(core|router)(.*)$': '<rootDir>/packages/sdk-$1/src/$2'
+    '@qbitapi/sdk-(core|router)(.*)$': '<rootDir>/packages/sdk-$1/src/$2'
   },
   testTimeout: 30000
 };
